abca47b01ce48dbb00dcaccad8ded9de6ea1b11e
Kate <polyakovkate@gmail.com>
2024-03-20 21:10:04 -0700

    added

---
+++ scripts/movie-api.js
@@ -0,0 +1,17 @@
+const IMDB_API_URL = 'http://www.omdbapi.com/';
+export class HackathonMovieApi {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+    this.movieURL = IMDB_API_URL;
+  }
+  async getMovieByTitle(moviename) {
+    try {
+      let movieResponse = axios.get(
+        `${this.movieURL}?t=${moviename}&apiKey=${this.apiKey}&plot=full`);
+      //console.log("data for kate", movieResponse.title);
+      return movieResponse;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
